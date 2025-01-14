f4af2209c20fc2d3946d5b363b6f1c5ed2fc6568
saloni_ghatiya <salonighatiya18@gmail.com>
2025-01-14 21:11:53 +0530

    comments on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image 
+  has_many :comments, dependent: :destroy
 
   validates :content, presence: true, unless: -> { image.attached? }
   validate :content_or_image_present
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable 
-  has_many :posts, dependent: :destroy # One user has many posts
+  has_many :posts, dependent: :destroy # One user has many posts 
+  has_many :comments, dependent: :destroy 
   validates :first_name, :last_name, :dob, :address, :mobile_number, :gender, presence: true
 end
